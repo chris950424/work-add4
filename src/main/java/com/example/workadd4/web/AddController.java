bb91c7792a7dfc72ad93b21e5823223acf8de176
unknown <chris950424@gmail.com>
2023-08-15 10:42:09 +0800

    Signed-off-by: unknown <chris950424@gmail.com>

--- src/main/java/com/example/workadd4/web/AddController.java
+++ src/main/java/com/example/workadd4/web/AddController.java
@@ -3,12 +3,17 @@ package com.example.workadd4.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AddController {
 
     @RequestMapping("/add")
-    public  Integer add(@RequestBody AddRequest addRequest){
-        
+    @ResponseBody
+    public String add(@RequestBody AddRequest addRequest) {
+        Integer num1 = addRequest.getNum1();
+        Integer num2 = addRequest.getNum2();
+        Integer sum = (num1 + num2);
+        return String.valueOf(sum);
     }
 }
