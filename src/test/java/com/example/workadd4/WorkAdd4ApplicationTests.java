57f78fa665da5352cd092ab3a2e1f7889abe1b64
unknown <chris950424@gmail.com>
2023-08-15 10:12:12 +0800

    first

---
+++ src/test/java/com/example/workadd4/WorkAdd4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workadd4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkAdd4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
